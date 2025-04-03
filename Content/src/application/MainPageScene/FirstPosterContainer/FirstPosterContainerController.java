250c930c0a25991b04175cf3869be4ee42f4f286
phuongtrieu97coder <phuongcoderinfinity@gmail.com>
2025-04-03 16:34:30 -0700

    cmit_04_03_2025_4:29:PM_phuongtrieu97coder
    +Contributors:
    
    Name: Trieu Phuong. GitHub Username: phuongtrieu97coder
    
    +Description:
    
    I have updated contents for 10 files such as Main.java, FirstPosterContainer.css, FirstPosterContainerController.java, FirstPosterContainerFXML.fxml, FirstPosterContainer_img1.png, TopNavigationBarFXML.fxml, module-info.java,  Phuong_design_plans_04_02_2025.docx, Modularize_your_project.docx, How_Eclipse_interacts_with_JavaFX_and_SceneBuilder.docx

---
+++ Content/src/application/MainPageScene/FirstPosterContainer/FirstPosterContainerController.java
@@ -0,0 +1,23 @@
+package application.MainPageScene.FirstPosterContainer;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+
+
+
+public class FirstPosterContainerController{
+	
+	@FXML
+	ImageView firstPosterContainer_firstPosterImage;
+	
+	@FXML
+	Rectangle firstPosterContainer_firstPosterImage_grayscaleBox;
+}
--- Content/src/module-info.java
+++ Content/src/module-info.java
@@ -4,5 +4,6 @@ module RQT_Real_Estate {
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens application.MainPageScene.TopNavigationBar to javafx.graphics, javafx.fxml;
+	opens application.MainPageScene.FirstPosterContainer to javafx.graphics, javafx.fxml;
 
 }

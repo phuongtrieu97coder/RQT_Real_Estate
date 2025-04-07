dfd762dd4ee87e4edb6ba4ce70dec3112a8114c8
phuongtrieu97coder <phuongcoderinfinity@gmail.com>
2025-04-06 19:44:32 -0700

    cmit_04_06_2025_7:36:PM_phuongtrieu97coder
    +Contributors:
    
    Name: Trieu Phuong
    
    GitHub Username: phuongtrieu97coder
    
    +Description:
    
    I have updated the contents for the Top Navigation Bar and the dropdown menu tab by making it slide up and down.
    
    I also changed the id of all the tools from this navigation bar with its dropdown menu tab.
    
    I removed the StackPane and AnchorPane from the top navigation bar.

--- Content/src/application/MainPageScene/TopNavigationBar/TopNavigationBarController.java
+++ Content/src/application/MainPageScene/TopNavigationBar/TopNavigationBarController.java
@@ -3,7 +3,7 @@ package application.MainPageScene.TopNavigationBar;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
@@ -12,109 +12,135 @@ import javafx.scene.control.Button;
 
 import javafx.scene.image.ImageView;
 
+import javafx.geometry.Insets;
+
 import javafx.animation.TranslateTransition;
-import javafx.animation.FadeTransition;
 
 import javafx.util.Duration;
 
 public class TopNavigationBarController{
 	@FXML
-	AnchorPane topNavigationBar_anchorPane1;
+	VBox topNavigationBar1_VBox1;
+	
+	@FXML
+	BorderPane topNavigationBar1_BorderPane1;
+	
+	@FXML
+	HBox topNavigationBar1_BorderPane1_Left_HBox1;
+	
+	@FXML
+	Button topNavigationBar1_BorderPane1_Left_HBox1_MenuButt;
 	
 	
 	@FXML
-	BorderPane topNavigationBar1;
-	
-	@FXML
-	HBox topNav1_leftContainer;
-	
-	@FXML
-	Button topNav1_leftContainer_dropdownMenu_butt;
-	
-	@FXML
-	HBox topNav1_leftContainer_dropdownMenu_butt_HBox;
-	
-	@FXML
-	ImageView topNav1_MiddleLogo;
+	ImageView topNavigationBar1_BorderPane1_Center_Logo;
 	
 	
 	@FXML
-	BorderPane topNav1_rightContainer;
-	
-	@FXML
-	HBox topNav1_rightContainer_HBox;
-	
-	@FXML
-	Button topNav1_rightContainer_HBox_SignIn_Butt;
-	
-	@FXML
-	Button topNav1_rightContainer_HBox_Help_Butt;
+	HBox topNavigationBar1_BorderPane1_Right_HBox1;
 	
 	
 	@FXML
-	HBox topNavigationBar_anchorPane1_dropdownMenuTab;
+	Button topNavigationBar1_BorderPane1_Right_HBox1_SignIn_Butt;
 	
 	@FXML
-	HBox topNavigationBar_anchorPane1_dropdownMenuTab_HBox1;
+	Button topNavigationBar1_BorderPane1_Right_HBox1_Help_Butt;
+	
 	
 	@FXML
-	Button topNavigationBar_anchorPane1_dropdownMenuTab_HBox1_Buy_Butt;
+	HBox topNavigationBar1_HBox1_Dropdown_Menu_Tab;
 	
 	@FXML
-	Button topNavigationBar_anchorPane1_dropdownMenuTab_HBox1_Rent_Butt;
+	HBox topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox1;
 	
 	@FXML
-	Button topNavigationBar_anchorPane1_dropdownMenuTab_HBox1_Sell_Butt;
+	Button topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox1_Buy_Butt;
 	
 	@FXML
-	Button topNavigationBar_anchorPane1_dropdownMenuTab_HBox1_Loan_Butt;
+	Button topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox1_Rent_Butt;
 	
 	@FXML
-	Button topNavigationBar_anchorPane1_dropdownMenuTab_HBox1_Agents_Butt;
+	Button topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox1_Sell_Butt;
 	
 	@FXML
-	Button topNavigationBar_anchorPane1_dropdownMenuTab_HBox1_News_Butt;
+	Button topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox1_Loan_Butt;
 	
 	@FXML
-	Button topNavigationBar_anchorPane1_dropdownMenuTab_Close_Butt;
+	Button topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox1_Agents_Butt;
+	
+	@FXML
+	Button topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox1_News_Butt;
+	
+	
+	@FXML
+	HBox topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox2;
+	
+	@FXML
+	Button topNavigationBar1_HBox1_Dropdown_Menu_Tab_HBox2_Close_Butt;
 	
 	public void initialize() {
-		topNavigationBar_anchorPane1_dropdownMenuTab.setTranslateY(-50);
-		topNavigationBar_anchorPane1_dropdownMenuTab.setVisible(false);
-		topNavigationBar_anchorPane1.getChildren().remove(topNavigationBar_anchorPane1_dropdownMenuTab);
+		topNavigationBar1_HBox1_Dropdown_Menu_Tab.setTranslateY(
+	    -topNavigationBar1_HBox1_Dropdown_Menu_Tab.getHeight());
+		
+		topNavigationBar1_HBox1_Dropdown_Menu_Tab.setVisible(false);
+		topNavigationBar1_VBox1.getChildren().remove(topNavigationBar1_HBox1_Dropdown_Menu_Tab);
 	}
 	
 	@FXML
-	public void show_topNavigationBar_anchorPane1_dropdownMenuTab() throws IOException{
-		TranslateTransition slideTransition = new TranslateTransition(Duration.millis(800), topNavigationBar_anchorPane1_dropdownMenuTab);
+	public void show_topNavigationBar1_HBox1_Dropdown_Menu_Tab() throws IOException{
+		TranslateTransition slideTransition = 
+	    new TranslateTransition(Duration.seconds(2), topNavigationBar1_HBox1_Dropdown_Menu_Tab);
 		
-		if(topNavigationBar_anchorPane1_dropdownMenuTab.isVisible()==false &&
-		   topNavigationBar_anchorPane1_dropdownMenuTab.getTranslateY() != 0) {
-			// If the tab is currently up, slide it down
-
+		
+		if(topNavigationBar1_HBox1_Dropdown_Menu_Tab.getTranslateY() == 
+		 -topNavigationBar1_HBox1_Dropdown_Menu_Tab.getHeight()) {
+			
+			// If the tab is currently up, slide it down, bring it to front, and expand the height of StackPane
+			
+			topNavigationBar1_VBox1.getChildren().add(topNavigationBar1_HBox1_Dropdown_Menu_Tab);
+			
+			slideTransition.setDelay(Duration.seconds(0.2));
+		    
+			topNavigationBar1_HBox1_Dropdown_Menu_Tab.setVisible(true);	
 			
 			slideTransition.setToY(0); // Target position (visible area)
 			slideTransition.play();
 			
 			
-			topNavigationBar_anchorPane1.getChildren().add(topNavigationBar_anchorPane1_dropdownMenuTab);
-			topNavigationBar_anchorPane1_dropdownMenuTab.setVisible(true);
 		}else {
-			// If the tab is currently down, slide it up
-
-			slideTransition.setToY(-50); // Target position (visible area)
+			
+			// If the tab is currently down, slide it up, bring it to back, and shrink the height of the StackPane
+			
+            slideTransition.setToY(-topNavigationBar1_HBox1_Dropdown_Menu_Tab.getHeight());
+            
+            slideTransition.setOnFinished(event -> {
+            	
+            	topNavigationBar1_HBox1_Dropdown_Menu_Tab.setVisible(false);
+            	topNavigationBar1_VBox1.getChildren().remove(topNavigationBar1_HBox1_Dropdown_Menu_Tab);
+            });
+			
 			slideTransition.play();
 			
-	        
-			topNavigationBar_anchorPane1.getChildren().remove(topNavigationBar_anchorPane1_dropdownMenuTab);
-			topNavigationBar_anchorPane1_dropdownMenuTab.setVisible(false);
 		}
 	}
 	@FXML
-	public void close_topNavigationBar_anchorPane1_dropdownMenuTab() throws IOException{
-		if(topNavigationBar_anchorPane1_dropdownMenuTab.isVisible()==true) {
-			topNavigationBar_anchorPane1.getChildren().remove(topNavigationBar_anchorPane1_dropdownMenuTab);
-			topNavigationBar_anchorPane1_dropdownMenuTab.setVisible(false);
+	public void close_topNavigationBar1_HBox1_Dropdown_Menu_Tab() throws IOException{
+		TranslateTransition slideTransition2 = new TranslateTransition(Duration.seconds(2), topNavigationBar1_HBox1_Dropdown_Menu_Tab);
+		
+		
+		if(topNavigationBar1_HBox1_Dropdown_Menu_Tab.getTranslateY() == 0) {
+			// If the tab is currently down, slide it up, bring it to back, and shrink the height of the StackPane
+
+			
+			slideTransition2.setToY(-topNavigationBar1_HBox1_Dropdown_Menu_Tab.getHeight());
+
+			 slideTransition2.setOnFinished(event -> {
+	            	
+            	topNavigationBar1_HBox1_Dropdown_Menu_Tab.setVisible(false);
+            	topNavigationBar1_VBox1.getChildren().remove(topNavigationBar1_HBox1_Dropdown_Menu_Tab);
+			 });
+			slideTransition2.play();
+			
 		}
 	}
 	
